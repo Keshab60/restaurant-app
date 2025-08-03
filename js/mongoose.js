b2252f7c7fa843780b05cb48774aabddd142782f
Keshab60 <keshabg500@gmail.com>
2025-08-03 15:59:51 +0530

    Update mongoose.js

--- js/mongoose.js
+++ js/mongoose.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const loginandsignup = mongoose.createConnection("mongodb://127.0.0.1:27017/loginandsignup", {
+const loginandsignup = mongoose.createConnection("https://restaurant-app-61ro.onrender.com/loginandsignup", {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 });
