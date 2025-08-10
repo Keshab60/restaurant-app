dd4f2e8e086b245830a8ebc04dc3d8f79f84503b
Keshab60 <keshabg500@gmail.com>
2025-08-10 19:12:35 +0530

    Update userprofile.js

--- js/userprofile.js
+++ js/userprofile.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+const userprofileimg = mongoose.createConnection("mongodb+srv://keshabg500:Keshab07112004@cluster0.vq06nt1.mongodb.net/userprofileimg", {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+});
+userprofile.on("connected", () => console.log("Connected to userprofile"));
 
+const userprofileimgschema = new mongoose.Schema({
+    id:{ type: Number, required: true },
+    Subtotal: { type: String, required: true },
+    DeliveryCharge: { type: String, required: true },
+    Total:{ type: String, required: true }
+
+
+});
+const profileIMG =userprofileimg.model("imageURL",userprofileimgschema  );
+module.exports =profileIMG;
